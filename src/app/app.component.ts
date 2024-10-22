ddbaecd9c12b912bd07bfb49ebd8f6860f249ceb
Javier Rodríguez <frodsot2402@g.educaand.es>
2024-10-22 19:51:55 +0200

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'UsoDirectivas';
+  people = [
+    { name: 'Paul' },
+    { name: 'John' },
+    { name: 'Lucy' },
+    { name: 'Nicholas' }
+  ];
+
+  
+  showList = true;
+  customStyle = false;
+
+  toggleShowList() {
+    this.showList = !this.showList;
+  }
+
+  toggleCustomStyle() {
+    this.customStyle = !this.customStyle;
+  }
 }
